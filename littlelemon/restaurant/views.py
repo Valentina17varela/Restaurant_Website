d2dab7a7df769a4330edbd978f417ee848dce531
Valentina Varela Alzate <valentina.varela17@outlook.com>
2023-09-28 08:55:39 -0500

    menu index

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Menu
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {'menu': menu_data}
+    return render(request, 'menu.html', main_data)
+
+
