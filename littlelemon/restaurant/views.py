dddb0efba27a12a0ab4eb272d0c9a747d6910422
Valentina17varela <valentina.varela17@outlook.com>
2024-01-15 17:40:17 -0500

    feature - navbar, about page

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,19 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 # Create your views here.
 def home(request):
-    template = loader.get_template('home.html')
+    template = loader.get_template("home.html")
     return HttpResponse(template.render())
 
+
 def about(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template("about.html")
+    return HttpResponse(template.render())
+
 
 def book(request):
     return HttpResponse("Hello world!")
 
+
 def menu(request):
     return HttpResponse("Hello world!")
+
 
 def menu_item(request):
     return HttpResponse("Hello world!")
