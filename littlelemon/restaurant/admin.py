dc717dd45836b9c284b376eb751941506ebbb558
Valentina17varela <valentina.varela17@outlook.com>
2024-01-17 01:53:55 -0500

    update readme description

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MenuItems, Book, Recomendation
+from .models import MenuItems, Book
 
 admin.site.register(MenuItems)
 admin.site.register(Book)
